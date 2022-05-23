2a1df9ac978d2847991eae35c81add16aa6aafb7
Jaivignesh Venugopal <jaivigneshvenugopal@gmail.com>
2022-05-24 06:50:25 +0800

    update

---
+++ update_dot_files.sh
@@ -0,0 +1,8 @@
+cp update_dot_files.sh dotfiles/update_dot_files.sh
+
+cp .bashrc dotfiles/bashrc
+cp .bash_profile dotfiles/bash_profile
+
+cp .vimrc dotfiles/vimrc
+
+cp .tmux.conf dotfiles/tmux.conf
